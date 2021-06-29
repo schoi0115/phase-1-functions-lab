0e5563ba3d8df1a9507c357102d41a4e6431011d
Shawn <schoi0115@gmail.com>
2021-06-29 00:19:48 +0000

    commit tree 379c292f94267a56cc1afcca96a83e7e2ffb92ad

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+const feet = 264
+
+function distanceFromHqInBlocks(Blocks) {
+    if (Blocks < 42){
+        return (Blocks - 42)*-1;
+    }
+    else return Blocks - 42
+}
+
+function distanceFromHqInFeet(Blocks) {
+    let Feet = distanceFromHqInBlocks(Blocks)
+        return Feet * 264
+
+}
+
+function distanceTravelledInFeet(startingBlock, endingBlock){
+   let distance =  Math.abs(endingBlock - startingBlock);
+   return distance*feet
+   
+}
+
+function calculatesFarePrice(start, destination)  {
+    let driveLength = (Math.abs(start - destination)*264);
+    if (driveLength <= 400) {
+        return 0;
+    }   else if (driveLength > 400 && driveLength <= 2000) {
+        return 0.02 * (driveLength - 400);
+    }   else if (driveLength > 2000 && driveLength < 2500) {
+        return  25;
+    }   else if (driveLength > 2500) {
+        return 'cannot travel that far';
+    }
+}
